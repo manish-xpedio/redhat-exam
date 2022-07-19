505e01796f1b4990314f528abb162410cb8aad00
mvbhavsar <mvbhavsar@hotmail.com>
2022-07-19 16:21:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+var cors = require('cors');
+var path = require('path');
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
+var formidable = require('formidable');
+var fs = require('fs');
+
+app.use(cors({
+	'allowedHeaders': ['sessionId', 'Content-Type'],
+	'exposedHeaders': ['sessionId'],
+	'origin': '*',
+	'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	'preflightContinue': false
+}));
+
+app.get('/', function(req, res) {
+	console.log('REQUEST', req.body);
+	res.send('Message from server....');
+})
+
+app.listen(7001);
+console.log('Server started on port 7001');
